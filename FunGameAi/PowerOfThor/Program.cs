85bea3ff8a88933687f7c9abbff9cd0cf36be26b
Dzen <hoggins.dev@gmail.com>
2020-05-02 22:39:45 +0300

    try thor puzzle; not shortest possible solution...

---
+++ FunGameAi/PowerOfThor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class P
+{
+static void Main()
+{
+var n=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+var (q,w)=(n[0]-n[2],n[1]-n[3]);
+int e=0xff&q,r=0xff&w;
+
+while(true)
+{
+Console.ReadLine();
+Console.WriteLine((--r<0?"":w<0?"N":"S")+(--e<0?"":q<0?"W":"E"));
+}
+}
+}
---
+++ FunGameAi/PowerOfThor/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PowerOfThor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PowerOfThor")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2C3EEE87-50B9-4EB5-B3D1-E5819D6A8D6A")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
