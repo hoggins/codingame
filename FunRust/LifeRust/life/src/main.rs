3cf4149c9741e1abc519c21717ec6448fdcdad7b
Dzen <hoggins.dev@gmail.com>
2020-09-19 21:59:05 +0300

    life: implement full set of goals

--- FunRust/LifeRust/life/src/main.rs
+++ FunRust/LifeRust/life/src/main.rs
@@ -1,5 +1,8 @@
+use std::cmp;
 use std::io;
+use std::ops::Add;
 use std::ops::Index;
+use std::ops::Sub;
 
 macro_rules! parse_input {
   ($x:expr, $t:ident) => {
@@ -23,35 +26,32 @@ fn main() {
   }
 
   let mut goals: Vec<Box<dyn Goal>> = Vec::new();
-  goals.push(Box::new(GoalGoToSample::new(0.1f64)));
-  goals.push(Box::new(GoalTakeSample::new(1f64)));
-  goals.push(Box::new(GoalGoToDiagnosis::new(0.1f64)));
-  goals.push(Box::new(GoalDiagnoseSample::new(1f64)));
+  goals.push(Box::new(GoalGoToSample::new(1)));
+  goals.push(Box::new(GoalTakeSample::new(10)));
+  goals.push(Box::new(GoalGoToDiagnosis::new(1)));
+  goals.push(Box::new(GoalDiagnoseSample::new(10)));
+  goals.push(Box::new(GoalGoToMolecules::new(1)));
+  goals.push(Box::new(GoalTakeMolecules::new(10)));
+  goals.push(Box::new(GoalGoToLab::new(10)));
+  goals.push(Box::new(GoalCompleteSample::new(10)));
 
   // game loop
   loop {
     let robots = input::parse_robots();
-
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let inputs = input_line.split(" ").collect::<Vec<_>>();
-    let available_a = parse_input!(inputs[0], i32);
-    let available_b = parse_input!(inputs[1], i32);
-    let available_c = parse_input!(inputs[2], i32);
-    let available_d = parse_input!(inputs[3], i32);
-    let available_e = parse_input!(inputs[4], i32);
-
+    let available = input::parse_available();
     let samples = input::parse_samples();
-
     let gs = GameState {
       robots: robots,
       samples: samples,
+      available: available,
     };
 
-    let mut best_weight = 0f64;
-    let mut best_goal: Option<&Box<Goal>> = None;
+    let mut best_weight = 0;
+    let mut best_goal: Option<&Box<_>> = None;
     for goal in goals.iter_mut() {
       let weight = goal.evaluate(&gs);
+      //eprintln!("{} weight {}", goal.name(), weight);
+
       if weight > best_weight {
         best_weight = weight;
         best_goal = Some(goal);
@@ -61,9 +61,7 @@ fn main() {
     if best_goal.is_some() {
       best_goal.unwrap().execute();
     } else {
-      let robots = gs.robots;
-      let samples = gs.samples;
-      try_think(robots, samples);
+      println!("WAIT");
     }
 
     // Write an action using println!("message...");
@@ -73,89 +71,8 @@ fn main() {
   }
 }
 
-fn try_think(robots: Vec<Robot>, samples: Vec<Sample>) {
-  let myself = robots.index(0);
-  let cur_sample = samples.iter().find(|&x| x.carried_by == 0);
-  if cur_sample.is_none() {
-    eprintln!("no sample");
-    println!("GOTO MOLECULES");
-    return;
-  }
-  let cur_sample = cur_sample.unwrap();
-  /*
-    if cur_sample.is_none() {
-      let sample_to_take = samples
-        .iter()
-        .filter(|&x| x.carried_by == -1)
-        .min_by_key(|&x| x.cost_a + x.cost_b + x.cost_c + x.cost_d + x.cost_e);
-      if sample_to_take.is_some() {
-        if myself.target != "DIAGNOSIS" {
-          println!("GOTO DIAGNOSIS");
-        } else {
-          let sample = sample_to_take.unwrap().sample_id;
-          println!("CONNECT {}", sample)
-        }
-      } else if myself.target != "SAMPLES" {
-        println!("GOTO SAMPLES");
-      } else {
-        println!("CONNECT 2");
-      }
-      return;
-    }
-
-    let cur_sample = cur_sample.unwrap();
-    if cur_sample.is_undiagnosed() {
-      if myself.target != "DIAGNOSIS" {
-        println!("GOTO DIAGNOSIS");
-      } else {
-        let sample = cur_sample.sample_id;
-        println!("CONNECT {}", sample)
-      }
-      return;
-    }
-  */
-  let c = cur_sample;
-  eprintln!(
-    "have sample a{} b{} c{} d{} e{}",
-    c.cost_a, c.cost_b, c.cost_c, c.cost_d, c.cost_e
-  );
-
-  {
-    let mut module: Option<String> = None;
-    if !is_enogh(myself.storage_a, cur_sample.cost_a - myself.expertise_a) {
-      module = Some("A".to_string());
-    } else if !is_enogh(myself.storage_b, cur_sample.cost_b - myself.expertise_b) {
-      module = Some("B".to_string());
-    } else if !is_enogh(myself.storage_c, cur_sample.cost_c - myself.expertise_c) {
-      module = Some("C".to_string());
-    } else if !is_enogh(myself.storage_d, cur_sample.cost_d - myself.expertise_d) {
-      module = Some("D".to_string());
-    } else if !is_enogh(myself.storage_e, cur_sample.cost_e - myself.expertise_e) {
-      module = Some("E".to_string());
-    }
-
-    if module.is_some() {
-      if myself.target != "MOLECULES" {
-        println!("GOTO MOLECULES");
-      } else {
-        println!("CONNECT {}", module.unwrap());
-      }
-      return;
-    }
-  }
-
-  if myself.target != "LABORATORY" {
-    println!("GOTO LABORATORY");
-  } else {
-    println!("CONNECT {}", cur_sample.sample_id);
-  }
-}
-
-fn is_enogh(storage: i32, cost: i32) -> bool {
-  storage >= cost
-}
-
 mod input {
+  use crate::MoleculeSet;
   use crate::Robot;
   use crate::Sample;
   use std::io;
@@ -170,16 +87,8 @@ mod input {
         target: inputs[0].trim().to_string(),
         eta: parse_input!(inputs[1], i32),
         score: parse_input!(inputs[2], i32),
-        storage_a: parse_input!(inputs[3], i32),
-        storage_b: parse_input!(inputs[4], i32),
-        storage_c: parse_input!(inputs[5], i32),
-        storage_d: parse_input!(inputs[6], i32),
-        storage_e: parse_input!(inputs[7], i32),
-        expertise_a: parse_input!(inputs[8], i32),
-        expertise_b: parse_input!(inputs[9], i32),
-        expertise_c: parse_input!(inputs[10], i32),
-        expertise_d: parse_input!(inputs[11], i32),
-        expertise_e: parse_input!(inputs[12], i32),
+        storage: MoleculeSet::new(&inputs[3..=7]),
+        expertise: MoleculeSet::new(&inputs[8..=12]),
       };
       robots.push(robot);
     }
@@ -201,27 +110,52 @@ mod input {
         rank: parse_input!(inputs[2], i32),
         expertise_gain: inputs[3].trim().to_string(),
         health: parse_input!(inputs[4], i32),
-        cost_a: parse_input!(inputs[5], i32),
-        cost_b: parse_input!(inputs[6], i32),
-        cost_c: parse_input!(inputs[7], i32),
-        cost_d: parse_input!(inputs[8], i32),
-        cost_e: parse_input!(inputs[9], i32),
+        cost: MoleculeSet::new(&inputs[5..=9]),
       };
 
       samples.push(sample);
     }
     samples
   }
+
+  pub fn parse_available() -> MoleculeSet {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    MoleculeSet::new(inputs.as_slice())
+  }
 }
 
 pub struct GameState {
   robots: Vec<Robot>,
   samples: Vec<Sample>,
+  available: MoleculeSet,
 }
 
 impl GameState {
   fn myself(&self) -> &Robot {
     self.robots.index(0)
+  }
+
+  fn get_completed_sample(&self) -> Option<&Sample> {
+    let myself = self.myself();
+    self
+      .samples
+      .iter()
+      .find(|&x| x.carried_by == 0 && x.is_diagnosed() && myself.missing_molecules(x).total() == 0)
+  }
+
+  fn get_best_to_collect(&self) -> Option<&Sample> {
+    let myself = self.myself();
+    self
+      .samples
+      .iter()
+      .filter(|&x| {
+        x.carried_by == 0
+          && x.is_diagnosed()
+          && self.available.can_substract(&myself.missing_molecules(x))
+      })
+      .min_by_key(|&x| myself.missing_molecules(x).total())
   }
 }
 
@@ -229,16 +163,14 @@ pub struct Robot {
   target: String,
   eta: i32,
   score: i32,
-  storage_a: i32,
-  storage_b: i32,
-  storage_c: i32,
-  storage_d: i32,
-  storage_e: i32,
-  expertise_a: i32,
-  expertise_b: i32,
-  expertise_c: i32,
-  expertise_d: i32,
-  expertise_e: i32,
+  storage: MoleculeSet,
+  expertise: MoleculeSet,
+}
+
+impl Robot {
+  pub fn missing_molecules(&self, s: &Sample) -> MoleculeSet {
+    ((s.cost - self.storage) - self.expertise).clamp_negative()
+  }
 }
 
 pub struct Sample {
@@ -247,47 +179,52 @@ pub struct Sample {
   rank: i32,
   expertise_gain: String,
   health: i32,
-  cost_a: i32,
-  cost_b: i32,
-  cost_c: i32,
-  cost_d: i32,
-  cost_e: i32,
+  cost: MoleculeSet,
 }
 
 impl Sample {
   pub fn is_undiagnosed(&self) -> bool {
-    self.cost_a == -1
+    self.cost.a == -1
     // && self.cost_b == 0 && self.cost_c == 0 && self.cost_d == 0 && self.cost_e == 0
+  }
+  pub fn is_diagnosed(&self) -> bool {
+    self.cost.a >= 0
   }
 }
 
 pub trait Goal {
-  fn evaluate(&mut self, gs: &GameState) -> f64;
+  fn name(&self) -> &'static str;
+  fn evaluate(&mut self, gs: &GameState) -> i32;
   fn execute(&self);
 }
 
 const SAMPLES: &str = "SAMPLES";
 const DIAGNOSIS: &str = "DIAGNOSIS";
+const MOLECULES: &str = "MOLECULES";
+const LABORATORY: &str = "LABORATORY";
 
 pub struct GoalTakeSample {
-  base_weight: f64,
+  base_weight: i32,
 }
 
 impl GoalTakeSample {
-  fn new(w: f64) -> GoalTakeSample {
+  fn new(w: i32) -> GoalTakeSample {
     GoalTakeSample { base_weight: w }
   }
 }
 
 impl Goal for GoalTakeSample {
-  fn evaluate(&mut self, gs: &GameState) -> f64 {
+  fn name(&self) -> &'static str {
+    "GoalTakeSample"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
     if gs.myself().target != SAMPLES {
-      return 0f64;
+      return 0;
     }
 
     let carring = gs.samples.iter().filter(|&x| x.carried_by == 0).count();
     if carring >= 3 {
-      return 0f64;
+      return 0;
     }
 
     self.base_weight
@@ -298,23 +235,26 @@ impl Goal for GoalTakeSample {
 }
 
 pub struct GoalGoToSample {
-  base_weight: f64,
+  base_weight: i32,
 }
 impl GoalGoToSample {
-  fn new(w: f64) -> GoalGoToSample {
+  fn new(w: i32) -> GoalGoToSample {
     GoalGoToSample { base_weight: w }
   }
 }
 
 impl Goal for GoalGoToSample {
-  fn evaluate(&mut self, gs: &GameState) -> f64 {
+  fn name(&self) -> &'static str {
+    "GoalGoToSample"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
     if gs.myself().target == SAMPLES {
-      return 0f64;
+      return 0;
     }
 
     let carring = gs.samples.iter().filter(|&x| x.carried_by == 0).count();
     if carring >= 3 {
-      return 0f64;
+      return 0;
     }
 
     self.base_weight
@@ -325,18 +265,21 @@ impl Goal for GoalGoToSample {
 }
 
 pub struct GoalGoToDiagnosis {
-  base_weight: f64,
+  base_weight: i32,
 }
 impl GoalGoToDiagnosis {
-  fn new(w: f64) -> GoalGoToDiagnosis {
+  fn new(w: i32) -> GoalGoToDiagnosis {
     GoalGoToDiagnosis { base_weight: w }
   }
 }
 
 impl Goal for GoalGoToDiagnosis {
-  fn evaluate(&mut self, gs: &GameState) -> f64 {
+  fn name(&self) -> &'static str {
+    "GoalGoToDiagnosis"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
     if gs.myself().target == DIAGNOSIS {
-      return 0f64;
+      return 0;
     }
 
     let carring = gs
@@ -345,7 +288,7 @@ impl Goal for GoalGoToDiagnosis {
       .filter(|&x| x.carried_by == 0 && x.is_undiagnosed())
       .count();
     if carring == 0 {
-      return 0f64;
+      return 0;
     }
 
     self.base_weight
@@ -356,11 +299,11 @@ impl Goal for GoalGoToDiagnosis {
 }
 
 pub struct GoalDiagnoseSample {
-  base_weight: f64,
+  base_weight: i32,
   to_diagnose: Option<i32>,
 }
 impl GoalDiagnoseSample {
-  fn new(w: f64) -> GoalDiagnoseSample {
+  fn new(w: i32) -> GoalDiagnoseSample {
     GoalDiagnoseSample {
       base_weight: w,
       to_diagnose: None,
@@ -369,9 +312,12 @@ impl GoalDiagnoseSample {
 }
 
 impl Goal for GoalDiagnoseSample {
-  fn evaluate(&mut self, gs: &GameState) -> f64 {
+  fn name(&self) -> &'static str {
+    "GoalDiagnoseSample"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
     if gs.myself().target != DIAGNOSIS {
-      return 0f64;
+      return 0;
     }
 
     self.to_diagnose = None;
@@ -380,7 +326,7 @@ impl Goal for GoalDiagnoseSample {
       .iter()
       .find(|&x| x.carried_by == 0 && x.is_undiagnosed());
     if to_diagnose.is_none() {
-      return 0f64;
+      return 0;
     }
     self.to_diagnose = Some(to_diagnose.unwrap().sample_id);
 
@@ -388,5 +334,246 @@ impl Goal for GoalDiagnoseSample {
   }
   fn execute(&self) {
     println!("CONNECT {}", self.to_diagnose.unwrap());
+  }
+}
+
+pub struct GoalGoToMolecules {
+  base_weight: i32,
+}
+impl GoalGoToMolecules {
+  fn new(w: i32) -> GoalGoToMolecules {
+    GoalGoToMolecules { base_weight: w }
+  }
+}
+
+impl Goal for GoalGoToMolecules {
+  fn name(&self) -> &'static str {
+    "GoalGoToMolecules"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
+    if gs.myself().target == MOLECULES {
+      return 0;
+    }
+
+    if gs.get_completed_sample().is_some() {
+      return 0;
+    }
+
+    if gs.get_best_to_collect().is_none() {
+      return 0;
+    }
+
+    self.base_weight
+  }
+  fn execute(&self) {
+    println!("GOTO {}", MOLECULES);
+  }
+}
+
+pub struct GoalTakeMolecules {
+  base_weight: i32,
+  molecule: Option<String>,
+}
+impl GoalTakeMolecules {
+  fn new(w: i32) -> GoalTakeMolecules {
+    GoalTakeMolecules {
+      base_weight: w,
+      molecule: None,
+    }
+  }
+}
+
+impl Goal for GoalTakeMolecules {
+  fn name(&self) -> &'static str {
+    "GoalTakeMolecules"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
+    self.molecule = None;
+    let myself = gs.myself();
+    if myself.target != MOLECULES {
+      return 0;
+    }
+
+    if gs.get_completed_sample().is_some() {
+      return 0;
+    }
+
+    let best = gs.get_best_to_collect();
+    if let Some(sample) = best {
+      self.molecule = Some(myself.missing_molecules(sample).first_name().to_string());
+      return self.base_weight;
+    }
+
+    eprintln!("no sample to fullfill");
+
+    eprintln!("available {:?}", gs.available);
+    eprintln!("storage {:?}", myself.storage);
+    eprintln!("expertise {:?}", myself.expertise);
+
+    for s in gs.samples.iter().filter(|&x| x.carried_by == 0) {
+      eprintln!("cost {:?}", s.cost);
+      eprintln!("missing {:?}", myself.missing_molecules(&s));
+      eprintln!("dif {:?}", gs.available - myself.missing_molecules(&s));
+      eprintln!("");
+    }
+
+    //panic!();
+    0
+  }
+  fn execute(&self) {
+    println!("CONNECT {}", self.molecule.as_ref().unwrap());
+  }
+}
+
+pub struct GoalGoToLab {
+  base_weight: i32,
+}
+impl GoalGoToLab {
+  fn new(w: i32) -> GoalGoToLab {
+    GoalGoToLab { base_weight: w }
+  }
+}
+
+impl Goal for GoalGoToLab {
+  fn name(&self) -> &'static str {
+    "GoalGoToLab"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
+    if gs.myself().target == LABORATORY {
+      return 0;
+    }
+
+    if gs.get_completed_sample().is_none() {
+      return 0;
+    }
+
+    self.base_weight
+  }
+
+  fn execute(&self) {
+    println!("GOTO {}", LABORATORY);
+  }
+}
+
+pub struct GoalCompleteSample {
+  base_weight: i32,
+  sample_id: Option<i32>,
+}
+impl GoalCompleteSample {
+  fn new(w: i32) -> GoalCompleteSample {
+    GoalCompleteSample {
+      base_weight: w,
+      sample_id: None,
+    }
+  }
+}
+
+impl Goal for GoalCompleteSample {
+  fn name(&self) -> &'static str {
+    "GoalCompleteSample"
+  }
+  fn evaluate(&mut self, gs: &GameState) -> i32 {
+    self.sample_id = None;
+    let myself = gs.myself();
+    if myself.target != LABORATORY {
+      return 0;
+    }
+
+    if let Some(completed) = gs.get_completed_sample() {
+      self.sample_id = Some(completed.sample_id);
+      return self.base_weight;
+    }
+
+    0
+  }
+  fn execute(&self) {
+    println!("CONNECT {}", self.sample_id.as_ref().unwrap());
+  }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct MoleculeSet {
+  a: i32,
+  b: i32,
+  c: i32,
+  d: i32,
+  e: i32,
+}
+
+impl MoleculeSet {
+  fn new(inputs: &[&str]) -> MoleculeSet {
+    MoleculeSet {
+      a: parse_input!(inputs[0], i32),
+      b: parse_input!(inputs[1], i32),
+      c: parse_input!(inputs[2], i32),
+      d: parse_input!(inputs[3], i32),
+      e: parse_input!(inputs[4], i32),
+    }
+  }
+  fn clamp_negative(self) -> MoleculeSet {
+    MoleculeSet {
+      a: cmp::max(0, self.a),
+      b: cmp::max(0, self.b),
+      c: cmp::max(0, self.c),
+      d: cmp::max(0, self.d),
+      e: cmp::max(0, self.e),
+    }
+  }
+
+  fn total(&self) -> i32 {
+    self.a + self.b + self.c + self.d + self.e
+  }
+
+  fn can_substract(&self, other: &MoleculeSet) -> bool {
+    self.a >= other.a
+      && self.b >= other.b
+      && self.c >= other.c
+      && self.d >= other.d
+      && self.e >= other.e
+  }
+
+  fn first_name(&self) -> &str {
+    if self.a > 0 {
+      return "A";
+    }
+    if self.b > 0 {
+      return "B";
+    }
+    if self.c > 0 {
+      return "C";
+    }
+    if self.d > 0 {
+      return "D";
+    }
+    if self.e > 0 {
+      return "E";
+    }
+    "U"
+  }
+}
+
+impl Add for MoleculeSet {
+  type Output = MoleculeSet;
+  fn add(self, other: MoleculeSet) -> <Self as std::ops::Add<MoleculeSet>>::Output {
+    MoleculeSet {
+      a: self.a + other.a,
+      b: self.b + other.b,
+      c: self.c + other.c,
+      d: self.d + other.d,
+      e: self.e + other.e,
+    }
+  }
+}
+
+impl Sub for MoleculeSet {
+  type Output = MoleculeSet;
+  fn sub(self, other: MoleculeSet) -> <Self as std::ops::Sub<MoleculeSet>>::Output {
+    MoleculeSet {
+      a: self.a - other.a,
+      b: self.b - other.b,
+      c: self.c - other.c,
+      d: self.d - other.d,
+      e: self.e - other.e,
+    }
   }
 }
